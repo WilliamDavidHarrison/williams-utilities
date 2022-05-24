9e1238696878f6260db9db169079d4225e90f8a4
William Harrison <william@williamdavidharrison.com.au>
2022-05-25 07:44:15 +0800

    Delete src directory

--- src/models/prefixSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-    _id: String,
-    prefix: String
-})
-
-module.exports = mongoose.model("prefixes", schema, "prefixes")
--- src/models/warnSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-    _id: String,
-    member: String,
-    warnings: Array
-})
-
-module.exports = mongoose.model("warnings", schema, "warnings")
