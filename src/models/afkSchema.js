d7640133cdd934964d40a5646ac39bb5c1e6816a
William Harrison <william@williamdavidharrison.com.au>
2022-06-04 13:24:45 +0800

    Delete src directory

--- src/models/afkSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-    _id: String,
-    afk: Boolean,
-    reason: String
-})
-
-module.exports = mongoose.model("afk", schema, "afk")
